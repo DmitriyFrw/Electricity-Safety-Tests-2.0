528e92adb5aae38a82a79e0f3dfc8cd750312d2e
DmitriyFrw <dmitriyfrw@gmail.com>
2026-06-05 09:41:23 +0300

    Delete grading.py

--- grading.py
+++
@@ -1,5 +0,0 @@
-from __future__ import annotations
-
-from app.support.grading import grade_css_class, grade_for_percent, score_percent
-
-__all__ = ["grade_css_class", "grade_for_percent", "score_percent"]
